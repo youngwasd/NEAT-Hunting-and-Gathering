98b2f912deabbe12d318babcdac5358980774f81
Gabe-Bryan <100544375+Gabe-Bryan@users.noreply.github.com>
2023-02-12 15:44:22 -0800

    undoing old changes for socket.io

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 var socket = null;
-if (window.io !== undefined) {
+if (window.io) {
 	console.log("Database connected!");
 
 	socket = io.connect('http://73.225.31.4:8888');
